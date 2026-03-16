6e60086161d2d8bb69878c210dd78048bf92d8ce
l-krrish <k2lala@uwaterloo.ca>
2026-03-16 12:00:37 -0400

    feat: add Block::new() and Block::genesis()

--- blockchain/src/block.rs
+++ blockchain/src/block.rs
@@ -8,3 +8,27 @@ pub struct Block{
     pub data : String,
     pub hash : String,
 }
+
+impl Block{
+    pub fn new(index : u64, prev_hash : String, data: String) -> Self{
+        Block{
+            index,
+            prev_hash,
+            data,
+            timestamp: 0,
+            nonce: 0,
+            hash: String::new()
+        }
+    }
+
+    pub fn genesis() -> Self{
+            Block{
+                index : 0,
+                prev_hash: String::from("0"),
+                data: String::from("Genesis Block"),
+                timestamp: 0,
+                nonce: 0,
+                hash: String::from("0")
+            }
+    }
+}
