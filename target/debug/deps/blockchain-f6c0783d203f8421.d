8072b678583ad5514bd0a399f7afc2000d48c0c2
l-krrish <k2lala@uwaterloo.ca>
2026-03-16 11:03:01 -0400

    chore: add .gitignore, untrack target/

--- target/debug/deps/blockchain-f6c0783d203f8421.d
+++
@@ -1,5 +0,0 @@
-/home/krrish-l/defi-chain/target/debug/deps/blockchain-f6c0783d203f8421.d: blockchain/src/lib.rs
-
-/home/krrish-l/defi-chain/target/debug/deps/libblockchain-f6c0783d203f8421.rmeta: blockchain/src/lib.rs
-
-blockchain/src/lib.rs:
