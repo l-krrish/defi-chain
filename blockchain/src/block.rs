7b4ca2c32f3cbfa343e05493156249a36e732515
l-krrish <k2lala@uwaterloo.ca>
2026-03-17 18:35:24 -0400

    feat: wire compute_hash into Block::new()

--- blockchain/src/block.rs
+++ blockchain/src/block.rs
@@ -1,3 +1,6 @@
+use sha2::{Digest, Sha256};
+use hex;
+
 #[derive(Debug, Clone)]
 
 pub struct Block{
@@ -13,11 +16,11 @@ impl Block{
     pub fn new(index : u64, prev_hash : String, data: String) -> Self{
         Block{
             index,
-            prev_hash,
-            data,
+            prev_hash : prev_hash.clone(),
+            data : data.clone(),
             timestamp: 0,
             nonce: 0,
-            hash: String::new()
+            hash: Block::compute_hash(index, 0, &prev_hash, 0,&data)
         }
     }
 
@@ -30,5 +33,15 @@ impl Block{
                 nonce: 0,
                 hash: String::from("0")
             }
+    }
+
+    pub fn compute_hash(index : u64, timestamp : i64, prev_hash : &str, nonce : u64, data : &str)-> String{
+        let mut hasher = Sha256::new();
+        hasher.update(index.to_string().as_bytes());
+        hasher.update(timestamp.to_string().as_bytes());
+        hasher.update(prev_hash.as_bytes());
+        hasher.update(nonce.to_string().as_bytes());
+        hasher.update(data.as_bytes());
+        hex::encode(hasher.finalize())
     }
 }
