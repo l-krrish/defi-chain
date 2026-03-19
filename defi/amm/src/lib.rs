f2a9292954eba49c4fa1d1a59e71c5db767549bb
l-krrish <k2lala@uwaterloo.ca>
2026-03-19 13:51:01 -0400

    feat: add swap_a_for_b() with 0.3% fee + passing unit test

--- defi/amm/src/lib.rs
+++ defi/amm/src/lib.rs
@@ -21,4 +21,29 @@ impl Pool{
         self.lp_supply += lp_minted;
         lp_minted
     }
+
+    pub fn swap_a_for_b(&mut self, amount_a: u64) -> u64{
+        let k = self.reserve_a * self.reserve_b;
+        let amount_a_with_fee = amount_a * 997 / 1000;
+        let new_reserve_a = self.reserve_a + amount_a_with_fee;
+        let new_reserve_b = k / new_reserve_a;
+        let output = self.reserve_b - new_reserve_b;
+        self.reserve_a = new_reserve_a;
+        self.reserve_b = new_reserve_b;
+        output
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_swap() {
+        let mut pool = Pool::new(100, 100_000);
+        let output = pool.swap_a_for_b(10);
+        println!("output: {}", output);
+        assert!(output > 8_000 && output < 9_000);
+
+    }
 }
