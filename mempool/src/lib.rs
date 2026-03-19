0985993f19a266331610126e189babe3f62ddb88
l-krrish <k2lala@uwaterloo.ca>
2026-03-19 09:45:11 -0400

    feat: add Mempool::new() and insert()

--- mempool/src/lib.rs
+++ mempool/src/lib.rs
@@ -11,3 +11,17 @@ pub struct Mempool{
     inner: BTreeMap<u64, Vec<PendingTx>>,
     ttl_secs: i64
 }
+
+impl Mempool{
+    pub fn new(ttl_secs: i64) -> Self{
+        Mempool{
+            inner: BTreeMap::new(),
+            ttl_secs,
+        }
+    }
+
+    pub fn insert(&mut self, tx: PendingTx){
+        let fee=tx.fee;
+        self.inner.entry(fee).or_default().push(tx);
+    }
+}
