d76cd5390ed16f4ee37a5ebfb6e9a85bb77839e0
l-krrish <k2lala@uwaterloo.ca>
2026-03-19 12:08:11 -0400

    feat: add pop_batch() and prune_expired() to Mempool

--- mempool/src/lib.rs
+++ mempool/src/lib.rs
@@ -24,4 +24,28 @@ impl Mempool{
         let fee=tx.fee;
         self.inner.entry(fee).or_default().push(tx);
     }
+
+   pub fn pop_batch(&mut self, n: usize) -> Vec<PendingTx> {
+    let mut batch = Vec::new();
+    while batch.len() < n {
+        if let Some((&fee, _)) = self.inner.iter().next_back() {
+            let txs = self.inner.get_mut(&fee).unwrap();
+            batch.push(txs.remove(0));
+            if txs.is_empty() {
+                self.inner.remove(&fee);
+            }
+        } else {
+            break;
+        }
+    }
+    batch
+}
+
+pub fn prune_expired(&mut self, now: i64){
+    for txs in self.inner.values_mut() {
+        txs.retain(|tx| now - tx.submitted_at < self.ttl_secs);
+    }
+    self.inner.retain(|_, v| !v.is_empty());
+}
+
 }
